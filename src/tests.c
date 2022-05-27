f35c42f40a6e90d77589b3aee3ff42d9e06eece4
AvirukBasak <avirukbasak@gmail.com>
2022-05-27 22:32:23 +0530

    updated src/tests.c

--- src/tests.c
+++ src/tests.c
@@ -1,3 +1,13 @@
-int main() {
+# include "headers/headers.h"
+# include "headers/errcodes.h"
+
+int main (int argsc, string argsv[])
+{
+    bool test_success = false;
+    if (!test_success) {
+        printf ("Test FAILED\n");
+    } else {
+        printf ("Test SUCCESS\n");
+    }
     return 0;
 }
