aa00d851158f6bf373fc61bef4ddf388a9be2752
AvirukBasak <avirukbasak@gmail.com>
2022-05-30 04:18:31 +0530

    added comments for PRI format specifiers

--- src/headers/headers.h
+++ src/headers/headers.h
@@ -12,21 +12,22 @@
     # define true                ( 1 )
     # define false               ( 0 )
 
-    typedef int8_t i8;
-    typedef int16_t i16;
-    typedef int32_t i32;
-    typedef int64_t i64;
+    typedef int8_t i8;      // PRIi8
+    typedef int16_t i16;    // PRIi16
+    typedef int32_t i32;    // PRIi32
+    typedef int64_t i64;    // PRIi64
 
-    typedef uint8_t u8;
-    typedef uint16_t u16;
-    typedef uint32_t u32;
-    typedef uint64_t u64;
+    typedef uint8_t u8;     // PRIu8
+    typedef uint16_t u16;   // PRIu16
+    typedef uint32_t u32;   // PRIu32
+    typedef uint64_t u64;   // PRIu64
 
-    typedef float f32;
-    typedef double f64;
+    typedef float f32;      // %f
+    typedef double f64;     // %lf
 
-    typedef int8_t byte;
-    typedef uint8_t bool;
-    typedef char *string;
+    typedef uint8_t bool;   // %s, PRIbool(bool)
+    typedef char *string;   // %s
+
+    # define PRIbool(b)     ( b ? "true" : "false" )
 
 # endif
